a08447baeb933b9e5e8fb28839e2328fabb0679a
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2023-01-03 01:19:04 -0800

    updated system to use a keymask for each key instead of a list of messages to apply to

--- generate.py
+++ generate.py
@@ -11,30 +11,48 @@ env = Environment(
 )
 
 
-def one_time_pad(message: str, key: str, direction: int = -1) -> str:
+def one_time_pad_character(message: str, key: str, direction: int = -1) -> str:
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    message = message.upper()
-    key = key.upper()
-    message_nums = [letters.index(s) if s in letters else -1 for s in message]
-    key_nums = [letters.index(s) if s in letters else -1 for s in key]
-    return "".join(
-        [
-            message[i]
-            if m == -1 or k == -1
-            else letters[(m + direction * k) % len(letters)]
-            for i, (m, k) in enumerate(zip(message_nums, key_nums))
-        ]
-    )
+    if message not in letters or key not in letters:
+        return message
+    m_num = letters.index(message)
+    k_num = letters.index(key)
+    out = letters[(m_num + direction * k_num) % len(letters)]
+    return out
 
 
-def encrypt_message(message: str, keys: list[str]) -> str:
-    for key in keys:
-        message = one_time_pad(message, key)
-    return message
+def encrypt_message(
+    message: str,
+    keys: dict[str, str],
+    keymasks: list[str],
+) -> str:
+    # initialize keys and output array, making everything uppercase
+    encrypted = list(message.upper())
+    keys = {name: key.upper() for name, key in keys.items()}
+
+    # keep track of where each key is at
+    key_indicies = {key: 0 for key in keys.keys()}
+
+    for i, char in enumerate(encrypted):
+        # apply all specified keys to the current character
+        for (key_name, key), mask in zip(keys.items(), keymasks.values()):
+            # only apply keys designated for this character
+            if mask[i] != "1":
+                continue
+
+            # get current key character, then advance counter
+            key_char = key[key_indicies[key_name]]
+            key_indicies[key_name] += 1
+            key_indicies[key_name] %= len(key)
+
+            # apply key
+            char = one_time_pad_character(char, key_char)
+        encrypted[i] = char
+    return "".join(encrypted)
 
 
 def get_hash(value: str) -> str:
-    return md5(value.lower().encode()).hexdigest()
+    return md5(value.upper().encode()).hexdigest()
 
 
 def generate(
@@ -54,41 +72,36 @@ def generate(
 
         # hashmap mapping code names to plaintext values
         # (should never be given to user)
-        codes = config["codes"]
+        keys = config["keys"]
 
-        # list of messages to encode
-        # (must all be shorter than their corresponding codes)
+        # message to encode
         # (should never be given to user)
-        messages = config["messages"]
+        message = config["message"]
 
-        # list of codes to be applied to each message
-        key_mappings = config["key_mapping"]
+        # mask determining which characters each key applies to
+        # can be given as-is to user
+        keymasks = config["keymasks"]
+        if len(keymasks) < len(keys):
+            raise ValueError("must have same number of keys and keymasks")
+        if any([len(message) < len(mask) for mask in keymasks]):
+            raise ValueError("all keymasks must have same length as message")
 
     # get stuff to be sent to user
 
     # get code names and hashes (safe to give to user)
     # the hash is used to give feedback when the user has the right code
-    code_names = list(codes.keys())
-    code_hashes = {name: get_hash(value) for name, value in codes.items()}
+    code_names = list(keys.keys())
+    code_hashes = {name: get_hash(value) for name, value in keys.items()}
 
-    # map each code to list of messages it applies to
-    code_appliers = {
-        name: [i for i, key in enumerate(key_mappings) if name in key]
-        for name in code_names
-    }
-
-    # encrypt messages (so they are safe to give to user)
-    message_keys = [[codes[name] for name in keys] for keys in key_mappings]
-    messages_encrypted = enumerate(
-        [encrypt_message(msg, key) for msg, key in zip(messages, message_keys)]
-    )
+    # encrypt message (so they are safe to give to user)
+    message_encrypted = enumerate(encrypt_message(message, keys, keymasks))
+    keymask_list = {name: list(value) for name, value in keymasks.items()}
 
     render = template.render(
         code_names=code_names,
-        messages=messages_encrypted,
+        message=message_encrypted,
         code_hashes=json.dumps(code_hashes),
-        code_appliers=json.dumps(code_appliers),
-        key_mappings=json.dumps(key_mappings),
+        keymasks=json.dumps(keymask_list),
     )
 
     with open(f"encryptor/build/{output_filename}", "w") as file:
